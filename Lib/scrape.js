2c8c6695fab3eacc07b533c29b89b4438c81d489
Raihanlimit <89056745+Raihanlimit@users.noreply.github.com>
2022-03-01 17:30:18 +0700

    Update scrape.js

--- Lib/scrape.js
+++ Lib/scrape.js
@@ -1251,7 +1251,7 @@ axios.post('https://twdown.net/download.php',qs.stringify({ URL })),{
 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 "cookie": "_ga=GA1.2.1388798541.1625064838; _gid=GA1.2.1351476739.1625064838; __gads=ID=7a60905ab10b2596-229566750eca0064:T=1625064837:RT=1625064837:S=ALNI_Mbg3GGC2b3oBVCUJt9UImup-j20Iw; _gat=1"
 	}
-})
+}
 .then(({ data }) => {
 const $ = cheerio.load(data)
 resolve({
